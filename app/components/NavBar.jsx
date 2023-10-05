faeb4672a5c2e3153ac1b751b7444aabc4b142ce
Samuel Mensah <samuelmensqh@gmail.com>
2023-10-05 03:52:35 +0000

    - mod size error

--- app/components/NavBar.jsx
+++ app/components/NavBar.jsx
@@ -61,7 +61,7 @@ export default function NavBar() {
           {/* Mobile view */}
           {
         
-            !istoggled ? <MenuIcon onClick={handleToggle} size={30sssss} className='block lg:hidden'/> : 
+            !istoggled ? <MenuIcon onClick={handleToggle} size={30} className='block lg:hidden'/> : 
             <X onClick={handleToggle} size={30} className='block lg:hidden'/>
           }
           {
